0cda02f5d1abbb71df6abace51a5cc29781c3f2b
groovypotato <gleasoncr@gmail.com>
2025-07-29 23:48:13 -0500

    added print_recipe_card, added max_length to Recipe class, added check object max_length when setting ingredients and instructions

--- src/main.py
+++ src/main.py
@@ -2,22 +2,19 @@ from recipe import Recipe
 
 
 test_recipe = Recipe("3-Ingredient Stovetop Mac and Cheese Recipe",
-                     "Kenji 3 ingredient Mac&Cheese",
                      "some ingredients",
                      "some instructions",
                      "some tags")
 
 test_ingredients = "6 ounces (170 g) elbow macaroni\nSalt\n6 ounces (180 ml) evaporated milk\n6 ounces (170 g) grated mild or medium cheddar cheese, or any good melting cheese, such as Fontina, Gruyère, or Jack"
-test_instructions = "1Place macaroni in a medium saucepan or skillet and add just enough cold water to cover.\n Add a pinch of salt and bring to a boil over high heat, stirring frequently.\n Continue to cook, stirring, until water has been almost completely absorbed and macaroni is just shy of al dente, about 6 minutes. \n Immediately add evaporated milk and bring to a boil. \nAdd cheese. \nReduce heat to low and cook, stirring continuously, until cheese is melted and liquid has reduced to a creamy sauce, about 2 minutes longer.\n Season to taste with more salt and serve immediately. "
+test_instructions = "Place macaroni in a medium saucepan or skillet and add just enough cold water to cover.\nAdd a pinch of salt and bring to a boil over high heat, stirring frequently.\nContinue to cook, stirring, until water has been almost completely absorbed and macaroni is just shy of al dente, about 6 minutes.\nImmediately add evaporated milk and bring to a boil.\nAdd cheese.\nReduce heat to low and cook, stirring continuously, until cheese is melted and liquid has reduced to a creamy sauce, about 2 minutes longer.\nSeason to taste with more salt and serve immediately. "
 test_tags = "Kenji\nCheese"
-
 
 def main():
   test_recipe.set_ingredients(test_ingredients)
   test_recipe.set_instructions(test_instructions)
   test_recipe.set_recipe_tags(test_tags)
-  print(repr(test_recipe))
-  print(test_recipe)
+  test_recipe.print_recipe_card()
 
 
 
--- src/recipe.py
+++ src/recipe.py
@@ -1,30 +1,58 @@
 class Recipe:
-    def __init__(self,title,description = None,ingredients= None, instructions = None, recipe_tags = None):
+    def __init__(self,title,ingredients= None, instructions = None, recipe_tags = None):
         self.title = title
-        self.description = description
         self.ingredients = ingredients
         self.instructions = instructions
         self.recipe_tags = recipe_tags
+        self.max_length = 0
 
     def __repr__(self):
         return f"Recipe({self.title},{self.description},{self.ingredients},{self.instructions},{self.recipe_tags})"
     
     def __str__(self):
         return f"{self.title}\n{self.description}\n{self.ingredients}\n{self.instructions}\n{self.recipe_tags}"
+    
 
+    def print_recipe_card(self):
+        title_card = self.title
+        if len(title_card) > self.max_length:
+            self.max_length = len(title_card)
+        self.max_length = self.max_length +5
+        top_bottom = "+" + "-" * (self.max_length - 2) + "+"
+        print(top_bottom)
+        print("| " + "* * * Recipe * * *".center(self.max_length - 4) + " |")
+        print("| " + " " * (self.max_length -4) + " |")
+        print("| " + title_card.ljust(self.max_length - 4) + " |")
+        print(top_bottom)
+        print("| " + "* * * Ingredients * * *".center(self.max_length - 4) + " |")
+        print(top_bottom)
+        for ingredients in self.ingredients:
+            print("| " + ingredients.ljust(self.max_length - 4) + " |")
+        print(top_bottom)
+        print("| " + "* * * Instructions * * *".center(self.max_length - 4) + " |")
+        print(top_bottom)
+        for instructions in self.instructions:
+             print("| " + instructions.ljust(self.max_length - 4) + " |")
+             print("| " + " " * (self.max_length -4) + " |")
+        print(top_bottom)
 
     def set_ingredients(self,text):
         ingredient_list = []
         ingredient_lines = text.splitlines()
         for ingredient in ingredient_lines:
             ingredient_list.append(ingredient)
+            if len(ingredient) > self.max_length:
+                 self.max_length = len(ingredient)
         self.ingredients = ingredient_list
+        
     
     def set_instructions(self,text):
             instructions_list = []
             split_instructions = text.splitlines()
             for instruction in split_instructions:
                 instructions_list.append(instruction)
+                if len(instruction) > self.max_length:
+                 self.max_length = len(instruction)
             self.instructions = instructions_list
 
     def set_recipe_tags(self,text):
