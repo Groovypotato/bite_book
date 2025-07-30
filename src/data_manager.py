0d3ee4cefd6e7a302dd8b4098c49eeb96ba13f51
groovypotato <gleasoncr@gmail.com>
2025-07-30 11:45:31 -0500

    Created load,add,save recipes_from_json to be able to manage the recipe storage. i also cereated a 5 recipe book that i can use to create search and recall to display

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,13 +1,23 @@
 import json
 
-file_path = "./static/recipes.json"
-
-
-
-def save_recipes_to_json(data):
+def save_recipes_to_json(data,file_path):
     try:
         with open(file_path, 'w') as book:
             json.dump(data, book, indent=2)
-    except Exception as e:
+    except FileNotFoundError as e:
         print(e)
 
+def load_recipes_from_json(file_path):
+    try:
+        with open(file_path,'r')as book:
+            data = json.load(book)
+        return data
+    except FileNotFoundError:
+        return {"recipes": []}
+
+def add_recipe_to_book(recipe_obj,file_path):
+    book = load_recipes_from_json(file_path)
+    if "recipes" not in book or not isinstance(book["recipes"], list):
+        book["recipes"] = []
+    book["recipes"].append(recipe_obj.recipe_to_dictionary())
+    save_recipes_to_json(book,file_path)
