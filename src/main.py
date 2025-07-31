d7ceecc9ec3ccc5cd4613bf280c7668c62820cb6
groovypotato <gleasoncr@gmail.com>
2025-07-31 10:39:21 -0500

    moved searching into run_saerch(), added entry menu, enclosed main in while loop

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def dictoinaries_to_recipes(list_of_dictionaries):
     list.append(dictionary_to_recipe(dictionary))
   return list
 
-def main():
+def run_search(file_path: str):
   user_search_type_question = input("What Type of Search do you want?: Title, Ingredient, Tag, or ID:    ")
   try:
      search_type = SearchType[user_search_type_question.upper()]
@@ -46,18 +46,22 @@ def main():
         user_search_again = input("Would you like to make another search?   'Yes' or 'No'    ")
         if user_search_again.lower() == "yes":
           print("Starting New Search")
+          run_search(file_path)
         elif user_search_again.lower() == "no":
-          print("Goodbye!")
-          exit()
+          pass
       elif user_print_card.lower() == "no":
         user_search_again = input("Would you like to make another search?   'Yes' or 'No'    ")
         if user_search_again.lower() == "yes":
           print("Starting New Search")
+          run_search(file_path)
         elif user_search_again.lower() == "no":
-          print("Goodbye!")
-          exit()
+          pass
       else:
         print("Incorrect Choice!")
+    elif int(user_multi_recipe_choice) == len(list_of_recipies)+1:
+      print("Canceling Search")
+    else:
+      print("Invalid Response!")
   elif len(results) == 1:
     i =1
     print(f"There was {i} Result:")
@@ -71,20 +75,37 @@ def main():
       user_search_again = input("Would you like to make another search?   'Yes' or 'No'    ")
       if user_search_again.lower() == "yes":
         print("Starting New Search")
+        run_search(file_path)
       elif user_search_again.lower() == "no":
-        print("Goodbye!")
-        exit()
+        print("Exiting to main menu...")
     elif user_print_card.lower() == "no":
       user_search_again = input("Would you like to make another search?   'Yes' or 'No'    ")
       if user_search_again.lower() == "yes":
          print("Starting New Search")
+         run_search(file_path)
       elif user_search_again.lower() == "no":
-        print("Goodbye!")
-        exit()
+        print("Exiting to main menu...")
+        
     else:
        print("Incorrect Choice!")
   else:
     print(f"0 recipes were found that match '{user_search_search_term}'")
+
+def main():
+  while True:
+    print("\nWelome to BiteBook, All your best recipes, byte-sized! ")
+    print("1. Search for recipe")
+    print("2. Add a new recipe")
+    print("3. Exit program")
+    user_feautre_choice = input("Choose an option:    ")
+    if int(user_feautre_choice) == 1:
+      run_search(file_path)
+    elif int(user_feautre_choice) == 2:
+      print("'Add recipe' feature coming soon!")
+    elif int(user_feautre_choice) == 3:
+      print("Goodbye")
+      exit()
+    
 
 
 
